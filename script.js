c48002c15fe3b3e13e74a173da516fb82478530a
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-02-19 20:31:27 -0500

    commiting symbols, characters and numbers

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+pwdElements = {
+
+  pwdLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  pwdUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  pwdCharacter: ["!", "@", "-", ".", "/", "#", ": ", "; ", "$", " % ", " & ", "\",  ", ",
+     "\", " < ", " > ","(", ")", " * ", " + ", "=", " ? ", "[", "]", " ^ ", "_",
+"`", "}", "|", "~"]
+
+
+}
 // Write password to the #password input
 function writePassword() {
+
+
   var password = generatePassword();
+
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
